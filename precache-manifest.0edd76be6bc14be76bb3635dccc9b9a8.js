bca3ac23a00a76f438cf1e3eb8e81b29391ce262
kyoung3212 <kevin.young3212@gmail.com>
2020-05-13 17:22:07 -0700

    Updates

---
+++ precache-manifest.0edd76be6bc14be76bb3635dccc9b9a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9f9a0efe04bbbb5cb76a92b51cf9eee",
+    "url": "/react-stand-app/index.html"
+  },
+  {
+    "revision": "5a6c9a4f59554d85e679",
+    "url": "/react-stand-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "502e9be38b68a88876a7",
+    "url": "/react-stand-app/static/js/2.851229a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-stand-app/static/js/2.851229a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a6c9a4f59554d85e679",
+    "url": "/react-stand-app/static/js/main.79fdcd88.chunk.js"
+  },
+  {
+    "revision": "216fc48e35ed4f17e0b2",
+    "url": "/react-stand-app/static/js/runtime-main.1a5bfd23.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-stand-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
