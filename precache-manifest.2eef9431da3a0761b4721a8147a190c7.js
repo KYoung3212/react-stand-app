3cb5f1c5218928f364f782d53563438685f802a7
kyoung3212 <kevin.young3212@gmail.com>
2020-05-13 17:23:05 -0700

    Updates

--- precache-manifest.2eef9431da3a0761b4721a8147a190c7.js
+++ precache-manifest.2eef9431da3a0761b4721a8147a190c7.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d9f9a0efe04bbbb5cb76a92b51cf9eee",
+    "revision": "e46d7e0a463a111a0145beac72658f83",
     "url": "/react-stand-app/index.html"
   },
   {
-    "revision": "5a6c9a4f59554d85e679",
-    "url": "/react-stand-app/static/css/main.5f361e03.chunk.css"
+    "revision": "5063f02c641809a43b6f",
+    "url": "/react-stand-app/static/css/main.b10c910b.chunk.css"
   },
   {
     "revision": "502e9be38b68a88876a7",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-stand-app/static/js/2.851229a2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5a6c9a4f59554d85e679",
-    "url": "/react-stand-app/static/js/main.79fdcd88.chunk.js"
+    "revision": "5063f02c641809a43b6f",
+    "url": "/react-stand-app/static/js/main.f8e571a8.chunk.js"
   },
   {
     "revision": "216fc48e35ed4f17e0b2",
@@ -26,5 +26,9 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
     "url": "/react-stand-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "bef4eddfc0366fa8671b355af2300b6c",
+    "url": "/react-stand-app/static/media/quest.bef4eddf.ttf"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-stand-app/precache-manifest.0edd76be6bc14be76bb3635dccc9b9a8.js"
+  "/react-stand-app/precache-manifest.2eef9431da3a0761b4721a8147a190c7.js"
 );
 
 self.addEventListener('message', (event) => {
